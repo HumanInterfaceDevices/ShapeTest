b30a609dca7300da7f99d8b292a1225eeb49b284
HumanInterfaceDevice <heavenisdigital@outlook.com>
2021-11-17 08:32:26 -0700

    attempting multiples

--- ShapeTest/ShapeTest/MainPage.xaml.cs
+++ ShapeTest/ShapeTest/MainPage.xaml.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using SkiaSharp;
 using SkiaSharp.Views.Forms;
+using System.Threading;
 
 namespace ShapeTest
 {
@@ -25,16 +26,7 @@ namespace ShapeTest
 			//});
 		}
 
-		SKPaint blackFill = new SKPaint
-		{
-			Style = SKPaintStyle.Fill,
-			Color = SKColors.Black
-		};
-		SKPaint redFill = new SKPaint
-		{
-			Style = SKPaintStyle.Fill,
-			Color = SKColors.DarkRed,
-		};
+		public static List<ShapeItem> shapeList = new List<ShapeItem>();
 
 
 		public const double pi = Math.PI;
@@ -44,41 +36,53 @@ namespace ShapeTest
 			//Prepare canvas
 			SKSurface surface = e.Surface;
 			SKCanvas canvas = surface.Canvas;
-			canvas.DrawPaint(redFill);
 
 			// canvas transforms
 			int width = e.Info.Width;
 			int height = e.Info.Height;
-			canvas.Translate(width / 2, height / 2);
-			canvas.Scale(Math.Min(width / 210f, height / 520f));
+			//canvas.Translate(width / 2, height / 2);
+			//canvas.Scale(Math.Min(width / 210f, height / 520f));
+			canvas.Scale(3);
 
-			// initialize
-			ShapeItem listShape = new ShapeItem();
-			listShape.Clear();
-
-			// create and draw shape
-			DrawScreen(canvas, listShape);
-
+			// create and draw the screen
+			DrawScreen(canvas);
 		}
-
-		private static void DrawScreen(SKCanvas canvas, ShapeItem listShape)
+		private static void DrawScreen(SKCanvas canvas)
 		{
-			SKPath bezierPath = SKPath.ParseSvgPathData(listShape.shape.svgPath);
-			SKColor fillColor = SKColor.Parse(listShape.shape.fill);
-			SKPaint paintFill = new SKPaint() { Style = SKPaintStyle.Fill, Color = fillColor, TextSize = 6 };
-			SKColor strokeColor = SKColor.Parse(listShape.shape.stroke);
-			SKPaint paintStroke = new SKPaint() { Style = SKPaintStyle.Stroke, Color = strokeColor };
+			
+			shapeList.Add(new ShapeItem());
+			shapeList.Add(new ShapeItem());
+			shapeList.Add(new ShapeItem());
+			shapeList.Add(new ShapeItem());
 
-			canvas.DrawPath(bezierPath, paintFill);
-			canvas.DrawPath(bezierPath, paintStroke);
-			SKPoint textPoint = new SKPoint(-150, 100);
-			SKPoint circlePoint = new SKPoint(0, 0);
+			double[] location = new double[2];
+			location[0] = 0; location[1] = 0;
+			foreach (ShapeItem shapeItem in shapeList)
+			{
+				
+				shapeItem.Clear();
+				shapeItem.shape.location[0] = location[0];
+				shapeItem.shape.location[1] = location[1];
+				SKPath bezierPath = SKPath.ParseSvgPathData(shapeItem.shape.svgPath);
+				//SKColor fillColor = SKColor.Parse(shapeItem.shape.fill);
+				SKColor fillColor = SKColor.Parse("ff" + Convert.ToString(111111 * (shapeItem.ID + 1)));
+				SKPaint paintFill = new SKPaint() { Style = SKPaintStyle.Fill, Color = fillColor, TextSize = Convert.ToSingle(2 * shapeItem.shape.height / 3) };
+				SKColor strokeColor = SKColor.Parse(shapeItem.shape.stroke);
+				SKPaint paintStroke = new SKPaint() { Style = SKPaintStyle.Stroke, Color = strokeColor, TextSize = Convert.ToSingle(2 * shapeItem.shape.height / 3) };
 
-			SKRect rect = new SKRect(0, 0, 200, 100);
-			canvas.DrawRect(rect, paintStroke);
-			canvas.DrawCircle(circlePoint, 2, paintFill);
-			//canvas.DrawText(listShape.shape.svgPath, textPoint, paintFill);
-			//Console.WriteLine(listShape.shape.svgPath);
+
+				canvas.DrawPath(bezierPath, paintFill);
+				canvas.DrawPath(bezierPath, paintStroke);
+				//SKPoint textPoint = new SKPoint(-150, 100);
+				SKPoint circlePoint = new SKPoint(0, 0);
+
+				SKRect rect = new SKRect(0, 0, Convert.ToSingle(shapeItem.shape.width), Convert.ToSingle(shapeItem.shape.height));
+				canvas.DrawRect(rect, paintStroke);
+				canvas.DrawCircle(circlePoint, 2, paintFill);
+				//canvas.DrawText(listShape.shape.svgPath, textPoint, paintFill);
+				//Console.WriteLine(listShape.shape.svgPath);
+				location[1] += shapeItem.shape.height;
+			}
 		}
 	}
 }
--- ShapeTest/ShapeTest/ShapeItem.cs
+++ ShapeTest/ShapeTest/ShapeItem.cs
@@ -7,6 +7,7 @@ using Newtonsoft.Json;
 
 namespace ShapeTest
 {
+	
 	public class ShapeItem
 	{
 		[PrimaryKey, AutoIncrement]
@@ -15,7 +16,6 @@ namespace ShapeTest
 		public List<int> children = new List<int> { 0 };
 		public string content = "";
 		public Shape shape = new Shape();
-
 		public void Clear()
 		{
 			parents.Clear();
@@ -29,8 +29,10 @@ public class Shape
 	{
 		public double height;
 		public double width;
-		public double margin;
-		public double padding;
+		public double scale;
+		public double[] location = new double[2];
+		public double[] margin = { 0, 0, 0, 0 };
+		public double[] padding = { 5, 5, 5, 5 };
 		public int facesLeft;
 		public int facesRight;
 		public double radiusLeft;
@@ -42,43 +44,58 @@ public class Shape
 
 		public void Clear()
 		{
-			height = 100;
-			width = 200;
-			margin = 0;
-			padding = 1;
-			facesLeft = 2;
+			height = 50;
+			width = 100;
+			location[0] = 0; location[1] = 0;
+			margin[0] = 2; margin[1] = 22; margin[2] = 2; margin[3] = 2;
+			padding[0] = 2; padding[1] = 2; padding[2] = 2; padding[3] = 2;
+			facesLeft = 8;
 			facesRight = 4;
-			radiusLeft = (height - (2 * padding)) / 10;
-			radiusRight = (height - (2 * padding)) / 4;
+			radiusLeft = (height - (padding[1] + padding[3])) / 3;
+			radiusRight = (height - (padding[1] + padding[3])) / 3;
 			svgPath = "";
-			fill = "ffffffff";
+			//fill = "ffffffff";
 			stroke = "ff000000";
 			pattern.Clear();
 			CreateSvgPath(this);
-
 		}
 
-		public void Clone(out Shape clone)
-		{
-			string serialized = JsonConvert.SerializeObject(this);
-			clone = JsonConvert.DeserializeObject<Shape>(serialized);
-		}
+		//public void Clone(out Shape clone)
+		//{
+		//	string serialized = JsonConvert.SerializeObject(this);
+		//	clone = JsonConvert.DeserializeObject<Shape>(serialized);
+		//}
 
 		public void CreateSvgPath(Shape shape)
 		{
-			string svgPath = " m " + Convert.ToString((shape.height / 2)) + " " + Convert.ToString(shape.padding);
-			float[,] pathPoint = new float[2, 4];
-			float offset = 0f;
-			double vectorStart = Math.PI;
-			// create left face path - counterclockwise, starting at top, direction = pi
-			double vectorCurrent = vectorStart; // direction of travel
-			FullArc arc = ArcPlot.ArcDivider(Math.PI, Math.PI / 2, shape.facesLeft + 1);
 
-			for (int j = 0; j < 2; j++) // creates left and bottom then right and top
+			if (shape.width == 0)
+			{
+				//SKRect bounds;
+				//canvas.GetLocalClipBounds(out bounds);
+				//shape.width = bounds.Width;
+			}
+			if (shape.height == 0)
+			{
+				//SKRect bounds;
+				//canvas.GetLocalClipBounds(out bounds);
+				//shape.height = bounds.Height;
+			}
+
+			double vectorCurrent = Math.PI; // starting at top, direction = pi
+			FullArc arc = ArcPlot.ArcDivider(Math.PI, Math.PI / 2, shape.facesLeft + 1);
+			double halfArcCos = Math.Abs(Math.Cos(arc.Angle / 2));
+			//how far away from the laft and right sides is the start of the end pieces
+			float offsetLeft = Convert.ToSingle((((shape.height - (shape.padding[1] + shape.padding[3])) / 2) / halfArcCos) - (shape.radiusLeft / halfArcCos) + shape.radiusLeft);
+			float offsetRight = Convert.ToSingle((((shape.height - (shape.padding[1] + shape.padding[3])) / 2) / halfArcCos) - (shape.radiusRight / halfArcCos) + shape.radiusRight);
+			string svgPath = " m " + (offsetLeft + shape.padding[0] + shape.location[0]) + " " + (shape.padding[1] + shape.location[1]);
+
+			float[,] pathPoint = new float[2, 4];
+			for (int j = 0; j <= 1; j++) // creates left and bottom then right and top
 			{
 				double radius = (j == 0) ? radiusLeft : radiusRight;
 				if (radius < 0) radius = 0;
-				double gap = (Math.Tan(arc.Angle/2) * (shape.height/2 - shape.padding)) - (Math.Tan(arc.Angle / 2) * radius); // gap = half of line between arcs
+				double gap = (Math.Tan(arc.Angle / 2) * ((shape.height - (shape.padding[1] + shape.padding[3])) / 2)) - (Math.Tan(arc.Angle / 2) * radius); // gap = half of line between arcs
 				for (int i = 0; i < arc.Count; i++)
 				{
 					pathPoint = ArcPlot.ArcPointsArray(vectorCurrent, arc.Angle, radius);
@@ -96,7 +113,8 @@ public class Shape
 					svgPath += " c " + pathPoint[0, 1] + " " + pathPoint[1, 1] + " " + pathPoint[0, 2] + " " + pathPoint[1, 2] + " " + pathPoint[0, 3] + " " + pathPoint[1, 3];
 					vectorCurrent -= arc.Angle;
 				}
-				pathPoint[0, 0] = Convert.ToSingle((Math.Cos(vectorCurrent) * gap) + (Math.Cos(vectorCurrent) * (shape.width - shape.height))); // gap after last curve
+				pathPoint[0, 0] = Convert.ToSingle(Math.Cos(vectorCurrent) * (shape.width - (offsetLeft + offsetRight) + gap - (shape.padding[0] + shape.padding[2])));
+				//pathPoint[0, 0] = Convert.ToSingle((Math.Cos(vectorCurrent) * gap) + (Math.Cos(vectorCurrent) * (shape.width - shape.height))); // gap after last curve
 				pathPoint[1, 0] = Convert.ToSingle(Math.Sin(vectorCurrent) * gap);
 				svgPath += " l " + pathPoint[0, 0] + " " + pathPoint[1, 0];
 				arc = ArcPlot.ArcDivider(Math.PI, Math.PI / 2, shape.facesRight + 1);
@@ -104,7 +122,6 @@ public class Shape
 			svgPath += " z ";
 			shape.svgPath = svgPath;
 		}
-
 	}
 }
 
